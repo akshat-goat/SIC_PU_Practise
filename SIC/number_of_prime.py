e7b808956dce46fd7464ecb3e0f8c061d138332d
akshat-goat <akshatforyou007@gmail.com>
2025-06-26 17:26:11 +0530

    done

---
+++ SIC/number_of_prime.py
@@ -0,0 +1,23 @@
+#Count the number of prime digits in a number
+def prime(n):
+    count=0
+    for i in range (2,n+1):
+        if n % i == 0 :
+            count = count + 1
+    if count == 1 :
+        return True
+    else :
+        return False
+
+
+
+number = input("Enter the Number : ")
+digits=[]
+prime_digits=[]
+for i in number:
+     digits.append(int(i))
+for i in digits:
+    if prime(i) == True:
+        prime_digits.append(int(i))
+print("Number of prime digits = ",len(prime_digits))
+
---
+++ SIC/prime_decreasing_order.py
@@ -0,0 +1,1 @@
+#Print the Prime numbers in decreasing order between m and n (m < n)
