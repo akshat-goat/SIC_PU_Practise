e1499730dde794361b66fe872228a8bd354d792f
akshat-goat <akshatforyou007@gmail.com>
2025-07-15 14:53:33 +0530

    more matplotlib programs done

--- libraries/bar_plot.py
+++ libraries/bar_plot.py
@@ -8,11 +8,11 @@ import seaborn as sns
 import pandas as pd
 from mpl_toolkits.mplot3d import Axes3D  # Importing 3D plotting module
 
-categories = ['A', 'B', 'C', 'D']
+cars = ['Ferrari', 'Lamborghini', 'Aston Martin', 'Dolorean']
 values = [10, 20, 15, 25]
 
-plt.bar(categories, values, color=['blue', 'green', 'red', 'purple'])
-plt.xlabel("Categories")
+plt.bar(cars, values, color=['blue', 'green', 'red', 'purple'])
+plt.xlabel("Cars")
 plt.ylabel("Values")
 plt.title("Bar Chart")
 plt.show()
